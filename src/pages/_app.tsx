68c3b801d08858eb8c45aa10a9a675e6597ce5ea
AjayiMike <ajayimikeofficial@gmail.com>
2023-09-10 23:01:34 +0100

    feat: wrap the app with redux Provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { reduxStore } from "@/state";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <Provider store={reduxStore}>
+            <Component {...pageProps} />
+        </Provider>
+    );
 }
